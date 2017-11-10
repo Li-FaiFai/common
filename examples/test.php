967913302808f45af2952b3815ea3f7f13ddf0de
Li-FaiFai <15011166177@163.com>
2017-11-10 14:21:46 +0800

    更新测试文件

---
+++ examples/test.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../autoload.php';
+
+use lifaifai\common\Test;
+
+$objTest = new Test();
+echo $objTest->helloWorld();
---
+++ src/Test.php
@@ -0,0 +1,9 @@
+<?php
+namespace lifaifai\common;
+
+class Test
+{
+	public static function helloWorld() {
+		echo 'hello world !';
+	}
+}
